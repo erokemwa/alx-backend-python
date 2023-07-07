4a092aefe549acbc7a16f839172d87723cd750af
erokemwa <111133254+erokemwa@users.noreply.github.com>
2023-07-07 04:17:45 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a function to concat two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns sum of two strings"""
+    return "{}{}".format(str1, str2)
